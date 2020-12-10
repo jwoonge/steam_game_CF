b89ecad0b5992e80d243b25ae3f870b8d1ef8182
jwoonge <511sjw@naver.com>
2020-12-10 15:11:30 +0900

    error detection

--- SteamDataCrawlingModule/FindValidUserByAPI.py
+++ SteamDataCrawlingModule/FindValidUserByAPI.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 key = '33C798A71E856B20EC314196A31C6AB6'
-start_user = 1000; end_user=20000; num_user = 0
+start_user = 0; end_user=100000; num_user = 0
 file_dir = 'results/userdata/'
 
 valid_user_list = open('results/valid_user_list.txt', 'a', encoding='utf-8')
@@ -14,21 +14,30 @@ def get_user_game_data(user_id):
     url += '&include_appinfo=true'
     url += '&include_played_free_games=true'
     url += '&format=json'
-    json = requests.get(url).json()
+    try :
+        json = requests.get(url).json()
 
-    if 'response' in json and not len(json['response'].keys())==0: #valid user
-        games = json['response']['games']
-        userdata = []
-        for game in games:
-            game_id = game['appid']
-            game_name = game['name']
-            game_playtime = game['playtime_forever']
-            userdata.append([game_id, game_name, game_playtime])
+        if 'response' in json and not len(json['response'].keys())==0: #valid user
+            try:
+                games = json['response']['games']
+                userdata = []
+                for game in games:
+                    game_id = game['appid']
+                    game_name = game['name']
+                    game_playtime = game['playtime_forever']
+                    userdata.append([game_id, game_name, game_playtime])
+                
+                userdata.sort(key=lambda x:x[2], reverse=True)
+                return userdata, True
             
-        userdata.sort(key=lambda x:x[2], reverse=True)
-        return userdata, True
+            except:
+                print(json)
+                return [], False
 
-    else:
+        else:
+            return [], False
+    except:
+        print('API ERROR')
         return [], False
 
 def write_user_game_data(user_id, game_data):
@@ -40,7 +49,7 @@ def write_user_game_data(user_id, game_data):
     f.close()
 
 for user_i in range(start_user, end_user):
-    user_id = str(user_i + 76561198120029537)
+    user_id = str(user_i + 76561197960265728)
     game_data, valid = get_user_game_data(user_id)
     if valid:
         valid_user_list.write(str(user_id)+'\n')
