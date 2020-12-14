df5a0115239aed144dbb131669364a68412902f8
jwoonge <511sjw@naver.com>
2020-12-15 01:54:32 +0900

    CF with user-tag matrix

--- CollaborativeFilteringModule/CalculateUserVectors.py
+++ CollaborativeFilteringModule/CalculateUserVectors.py
@@ -1,5 +1,6 @@
 import os
 import csv
+from collections import namedtuple
 MAXV = 999999
 user_data_dir = '../SteamDataCrawlingModule/results/userdata/'
 user_data_dir_test = 'fortesting/'
@@ -7,15 +8,23 @@ user_data_dir_test = 'fortesting/'
 def get_game_data(filename='gamedata.txt'):
     game_datas = dict()
     f = open(filename, 'r',encoding='utf-8')
+    game_tag_range = set()
     for row in f:
-        game_id, game_name, avg_playtime, score = row.split('##')
-        avg_playtime = float(avg_playtime[:-1])
-        game_datas[game_id] = [game_name, avg_playtime, score]
+        game_id, game_name, avg_playtime, score, tags = row.split('##')
+        avg_playtime = float(avg_playtime)
+        score = float(score)
+        tags = tags[:-1].split('$$')
+        game_tag_range = game_tag_range.union(set(tags))
+        tmp = namedtuple('game_data',['game_name', 'avg_playtime','recommend_rate', 'tags'])
+        tmp.game_name = game_name; tmp.avg_playtime = avg_playtime; tmp.recommend_rate = score; tmp.tags = tags
+        game_datas[game_id] = tmp
+    game_tag_range.remove('')
     f.close()
-    return game_datas
+    return game_datas, list(game_datas.keys()), list(game_tag_range)
 
-def get_user_dict(user_id):
-    user_dict = dict()
+def get_user_dict(user_id, game_datas):
+    user_game_dict = dict()
+    user_tag_dict = dict()
     f = open(user_data_dir + user_id + '.txt', 'r', encoding='utf-8')
     GR = 'game'
     for row in f:
@@ -27,45 +36,70 @@ def get_user_dict(user_id):
             if GR == 'game':
                 game_id, game_name, game_playtime = row.split('##')
                 game_playtime = float(game_playtime[:-1])
-                user_dict[game_id] = game_playtime
+                if game_id in game_datas:
+                    game_tags = game_datas[game_id].tags
+                    for tag in game_tags:
+                        if tag in user_tag_dict:
+                            user_tag_dict[tag] += game_playtime
+                        else:
+                            user_tag_dict[tag] = game_playtime
+                user_game_dict[game_id] = game_playtime
             else:
                 game_id, rec = row.split('##')
                 if 'Not' in rec:
-                    user_dict[game_id] = -MAXV
+                    user_game_dict[game_id] = - MAXV
                 else:
-                    user_dict[game_id] = MAXV
-    return user_dict
+                    user_game_dict[game_id] = MAXV
+    return user_game_dict, user_tag_dict
 
-def cal_user_vector(user_dict):
-    vector = []
+def cal_user_game_vector(user_game_dict, game_range):
+    user_game_vector = []
     vector_max = 0
     for game_id in game_range:
-        if game_id in user_dict:
-            if user_dict[game_id] == MAXV:
-                vector.append(MAXV)
-            elif user_dict[game_id] == -MAXV:
-                vector.append(-5)
+        if game_id in user_game_dict:
+            if user_game_dict[game_id] == MAXV:
+                user_game_vector.append(MAXV)
+            elif user_game_dict[game_id] == -MAXV:
+                user_game_vector.append(-5)
             else:
-                value = user_dict[game_id] / game_datas[game_id][1]
+                value = user_game_dict[game_id] / game_datas[game_id].avg_playtime
                 if value > vector_max:
                     vector_max = value
-                vector.append(value)
+                user_game_vector.append(value)
         else:
-            vector.append(0)
-    for i in range(len(vector)):
-        if vector[i]==MAXV:
-            vector[i] = vector_max
-    return vector
+            user_game_vector.append(0)
+    for i in range(len(user_game_vector)):
+        if user_game_vector[i]==MAXV:
+            user_game_vector[i] = vector_max
+    return user_game_vector
+
+def cal_user_tag_vector(user_tag_dict, tag_range):
+    user_tag_vector = []
+    for tag in tag_range:
+        if tag in user_tag_dict:
+            user_tag_vector.append(user_tag_dict[tag])
+        else:
+            user_tag_vector.append(0)
+    return user_tag_vector
+        
+
+
+
 
 if __name__=='__main__':
-    game_datas = get_game_data()
-    game_range = list(game_datas.keys())
-    f = open('uservectors.csv', 'w', newline='\n')
-    w = csv.writer(f)
+    game_datas, game_range, tag_range = get_game_data()
+    gf = open('user_game_vectors.csv', 'w', newline='\n')
+    tf = open('user_tag_vectors.csv', 'w', newline='\n')
+    gw = csv.writer(gf)
+    tw = csv.writer(tf)
     #for filename in os.listdir(user_data_dir):
     for filename in os.listdir(user_data_dir_test):
         user_id = filename[:-4]
-        user_dict = get_user_dict(user_id)
-        user_vector = cal_user_vector(user_dict)
-        w.writerow(user_vector)
-    f.close()
+        user_game_dict, user_tag_dict = get_user_dict(user_id, game_datas)
+        user_game_vector = cal_user_game_vector(user_game_dict, game_range)
+        user_tag_vector = cal_user_tag_vector(user_tag_dict, tag_range)
+        print(user_tag_vector)
+        gw.writerow(user_game_vector)
+        tw.writerow(user_tag_vector)
+    gf.close()
+    tf.close()
--- CollaborativeFilteringModule/UserBasedCF.py
+++ CollaborativeFilteringModule/UserBasedCF.py
@@ -2,23 +2,64 @@ import csv
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from CalculateUserVectors import get_game_data
+'''
+def retrieve():
+    return 추천하는 game_id
+def evaluate():
+    print(precision & recall)
+'''
 
-game_datas = get_game_data()
-user_vectors = np.array(np.loadtxt('uservectors.csv', dtype=np.float, delimiter=','))
-test_set = user_vectors[:5, :]
-train_set = user_vectors[5:,:]
-sim = cosine_similarity(test_set, train_set)
-sim_user = np.argmax(sim, axis=1)
+def normalize(vectors):
+    mean_of_col = np.sum(vectors, axis=0)
+    count_nonzero = np.count_nonzero(vectors, axis=0)
+    count_nonzero = np.where(count_nonzero==0, 100, count_nonzero)
+    mean_of_col = mean_of_col / count_nonzero
+    mean_of_col = np.where(mean_of_col==0, 0.01, mean_of_col)
+    return vectors / mean_of_col
 
-def most_sim_user_vector(train_set, test_set):
+def most_sim_user_index(train_set, test_set):
     sim = cosine_similarity(test_set, train_set)
     sim_user_index = np.argmax(sim, axis=1)
-    return train_set[sim_user_index]
+    return sim_user_index
 
-test_set_t = np.array([[0,0,1], [1,0,0], [0,1,1]])
-train_set_t = np.array([[0,0,5], [1,1,0], [1,1,1], [0,1,2]])
 
-sim_user = most_sim_user_vector(train_set_t, test_set_t)
-print(sim_user)
+game_datas, game_range, tag_range = get_game_data()
+game_range = np.array(game_range); tag_range = np.array(tag_range)
+recommend_rates = []
+for i in range(len(game_range)):
+    game_id = game_range[i]
+    recommend_rates.append(game_datas[game_id].recommend_rate)
+recommend_rates = np.array(recommend_rates)
 
-#print(cos_sim(train_set, test_set.T))
+## Read Data & Normalize
+user_game_vectors = np.array(np.loadtxt('user_game_vectors.csv', dtype=np.float, delimiter=','))
+user_tag_vectors = np.array(np.loadtxt('user_tag_vectors.csv', dtype=np.float, delimiter=','))
+user_tag_vectors = normalize(user_tag_vectors)
+## Separate test-train set
+test_set_g = user_game_vectors[:5, :]
+train_set_g = user_game_vectors[5:,:]
+test_set_t = user_tag_vectors[:5, :]
+train_set_t = user_tag_vectors[5:,:]
+## Find most similar user
+sim_user_index_t = most_sim_user_index(train_set_t, test_set_t)
+print(sim_user_index_t)
+sim_user_vector = train_set_g[sim_user_index_t]
+sim_user_vector = sim_user_vector * recommend_rates
+
+
+'''
+sim_user_index_g = most_sim_user_index(train_set_g, test_set_g)
+sim_user_vector_g = train_set_g[sim_user_index_g]
+sim_user_vector_g = sim_user_vector_g * recommend_rates
+'''
+recommend_game_index = np.argmax(sim_user_vector, axis=1)
+print(recommend_game_index)
+recommand_game_id = game_range[recommend_game_index]
+real_game_index = np.argmax(test_set_g, axis=1)
+real_game_id = game_range[real_game_index]
+print(recommand_game_id)
+for i in range(len(recommand_game_id)):
+    print('추천게임:',game_datas[recommand_game_id[i]].game_name)
+    print('실제게임:',game_datas[real_game_id[i]].game_name)
+    print('--')
+    print(tag_range[np.argmax(test_set_t[i])])
